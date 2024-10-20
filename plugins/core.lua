3364a5194cd0628d8c113386acabbf2b24820941
NiGHT <86895149+NiGHT757@users.noreply.github.com>
2024-10-20 03:07:37 +0300

    v1.0.0

---
+++ plugins/core.lua
@@ -0,0 +1,15 @@
+function GetPluginAuthor()
+    return "NiGHT"
+end
+
+function GetPluginVersion()
+    return "1.0.0"
+end
+
+function GetPluginName()
+    return "TeamBet"
+end
+
+function GetPluginWebsite()
+    return "github.com/NiGHT757/swiftly-teambet"
+end
